9629b838198013b713b68b1280891c6c814da907
lgiannes <lorenzo.giannessi@unige.ch>
2023-03-13 20:39:16 +0100

    bug fixed (missing line)

--- scripts/FCT/Linux/setup.sh
+++ scripts/FCT/Linux/setup.sh
@@ -1,3 +1,4 @@
+export GENERALDATADIR="/home/neutrino/FCT/FCT_database/"
 [ ! -d $GENERALDATADIR ] && mkdir $GENERALDATADIR
 export GUI_FOLDER="/home/neutrino/FCT/GUI_UT92"
 export FCT_RUN_FOLDER="/home/neutrino/FCT/code/scripts/FCT/Linux"

0d79ead76addd3fb07899d09faa5740cc56019d9
lgiannes <lorenzo.giannessi@unige.ch>
2023-06-09 15:26:05 +0200

    increased max HV to 58 V in HV test

--- scripts/FCT/Linux/utils/LBHK_fromscript_part2.cs
+++ scripts/FCT/Linux/utils/LBHK_fromscript_part2.cs
@@ -362,15 +362,15 @@ bool HouseKeeping_test(string OutFile_Name,byte FEB_BoardID,string config_path, 
     bool HV_ADC_success = true;
 
     // Set high voltages
-    double HighestHV = 35;//V
+    double HighestHV = 58;//V
     double[] HVs_volts = new double[8];
     // Test two different HV value for each ASIC. -> two loops
     for(int i=0;i<8;i++){
-        HVs_volts[i] = HighestHV/8*(i)+1;
+        HVs_volts[i] = HighestHV/8*(i)+HighestHV/8;
     }
     HV_ADC_success1 = HV_test(HVs_volts,OutFile_Name, HK_values);
     for(int i=0;i<8;i++){
-        HVs_volts[i] = HighestHV/8*(7-i)+1;
+        HVs_volts[i] = HighestHV/8*(7-i)+HighestHV/8;
     }
     HV_ADC_success2 = HV_test(HVs_volts,OutFile_Name, HK_values);
     // SUCCESS if both are successful
@@ -497,15 +497,15 @@ bool MPPCTemp_test(string OutFile_Name){
 bool MPPCHV_test(string OutFile_Name){
     // Set high voltages
     bool HV_ADC_success,HV_ADC_success1,HV_ADC_success2;
-    double HighestHV = 35;//V
+    double HighestHV = 58;//V
     double[] HVs_volts = new double[8];
     // Test two different HV value for each ASIC. -> two loops
     for(int i=0;i<8;i++){
-        HVs_volts[i] = HighestHV/8*(i)+1;
+        HVs_volts[i] = HighestHV/8*(i)+HighestHV/8;
     }
     HV_ADC_success1 = HV_test_FEBside(HVs_volts,OutFile_Name);
     for(int i=0;i<8;i++){
-        HVs_volts[i] = HighestHV/8*(7-i)+1;
+        HVs_volts[i] = HighestHV/8*(7-i)+HighestHV/8;
     }
     HV_ADC_success2 = HV_test_FEBside(HVs_volts,OutFile_Name);
     // SUCCESS if both are successful

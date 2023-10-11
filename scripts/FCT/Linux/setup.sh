ae793804e8699323a95acfe167e2b157d8cbbf1c
lgiannes <lorenzo.giannessi@unige.ch>
2023-10-11 11:10:42 +0200

    push to setup_unige branch the setup file

--- scripts/FCT/Linux/setup.sh
+++ scripts/FCT/Linux/setup.sh
@@ -1,17 +1,42 @@
-export GENERALDATADIR="/home/neutrino/FCT/FCT_database/"
+### Mother directory
+#export GENERALDATADIR="/DATA/FCT_retests_y/"
+#export GENERALDATADIR="/DATA/FCT_WITH_COLDPLATES/"
+export GENERALDATADIR="/DATA/FCT_RETEST_FEB_FROMJAPAN/"
+
+### other directories settings
 [ ! -d $GENERALDATADIR ] && mkdir $GENERALDATADIR
-export GUI_FOLDER="/home/neutrino/FCT/GUI_UT92"
-export FCT_RUN_FOLDER="/home/neutrino/FCT/code/scripts/FCT/Linux"
+export GUI_FOLDER="/home/neutrino/FCT/GUI"
+export FCT_RUN_FOLDER="/home/neutrino/FCT/software/GUIscripts/scripts/FCT/Linux"
+export FCT_UTILS=$FCT_RUN_FOLDER"/utils/"
 export MIBDATADIR=$GENERALDATADIR"/MIBs/"
 [ ! -d $MIBDATADIR ] && mkdir $MIBDATADIR
 export FEBDATADIR=$GENERALDATADIR"/FEBs/"
 [ ! -d $FEBDATADIR ] && mkdir $FEBDATADIR
+export ANALYSIS_FOLDER="/home/neutrino/FCT/software/FunctionalTest/"
+export CONFIGFOLDER="/home/neutrino/FCT/software/GUIscripts/config/"
+export GPIO_CALIB_FOLDER=$FCT_RUN_FOLDER"/GPIO_calib"
 
-export ANALYSIS_FOLDER="/home/neutrino/FCT/FunctionalTest/"
-export CONFIGFOLDER="/home/neutrino/FCT/code/config/"
-echo $GENERALDATADIR
+### the following should match the IP address and PORT indicated on the app-settings.json file (lines 116-122) in the GUI folder
+export ip_address="10.195.52.177"
+#export ip_address="10.195.52.144"
+#export port="11000"
+export port="12000"
 
-#export ip_address="10.195.52.177"
-export ip_address="10.195.52.144"
-export port="11000"
-#export port="12000"
+export GPIO_SN="44"
+
+
+export PS_HV="60"
+
+### test configuration 
+#                       Default: 
+#                               CONFIGNAME="config_FCT2_newGUI_V2.xml"
+#                               amp="0.030" #V
+#                               HG="12"
+#                               LG="56"
+#                               DAC10B="300"
+
+export CONFIGNAME="config_FCT2_newGUI_V2.xml"
+export amp="0.030" #V
+export HG="12"
+export LG="56"
+export DAC10B="300"

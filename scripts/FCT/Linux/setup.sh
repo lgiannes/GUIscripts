625446b196669ba309374c2d6732c706f4266f13
lgiannes <lorenzo.giannessi@unige.ch>
2023-10-11 15:52:44 +0200

    updated setup.sh with comments

--- scripts/FCT/Linux/setup.sh
+++ scripts/FCT/Linux/setup.sh
@@ -16,7 +16,7 @@ export ANALYSIS_FOLDER="/home/neutrino/FCT/software/FunctionalTest/"
 export CONFIGFOLDER="/home/neutrino/FCT/software/GUIscripts/config/"
 export GPIO_CALIB_FOLDER=$FCT_RUN_FOLDER"/GPIO_calib"
 
-export WHICHSETUP="UNIGE"
+export WHICHSETUP="UNIGE" # use ONLY "UNIGE" or "JPARC"
 
 ### the following should match the IP address and PORT indicated on the app-settings.json file (lines 116-122) in the GUI folder
 export ip_address="10.195.52.177"
@@ -24,7 +24,10 @@ export ip_address="10.195.52.177"
 #export port="11000"
 export port="12000"
 
+#check GPIO SN on the board
 export GPIO_SN="44"
+#max HV from the PS
 export PS_HV="60"
 
+#Load the configuration settings
 source test_config.sh
